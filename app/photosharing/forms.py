c2de378b238f8bf9e41e31be328764e50391ada3
ryangmolina <ryangabrielpmolina@gmail.com>
2018-10-24 21:55:15 +0800

    Add TeamCreateForm

--- app/photosharing/forms.py
+++ app/photosharing/forms.py
@@ -1,9 +1,17 @@
 from django.forms import ModelForm
 
-from .models import Photo
+from .models import Photo, Team
 
-
+# Forms can be created like this, then pass it as a form_class for FormView
+# But, it's better to use CreateView when dealing with creation of objects.
+# FormView with form_class must only be used on forms like Contact-Us
 class PhotoUploadForm(ModelForm):
     class Meta:
         model = Photo
         fields = ('photo', 'description', 'public', )
+
+
+class TeamCreateForm(ModelForm):
+    class Meta:
+        model = Team
+        fields = ('logo', 'name', 'description')
