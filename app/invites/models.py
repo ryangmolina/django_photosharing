6e60fbc5ba167c42ab2cd40f66935eefb7fac1c7
ryangmolina <ryangabrielpmolina@gmail.com>
2018-10-24 21:53:59 +0800

    Update code format

--- app/invites/models.py
+++ app/invites/models.py
@@ -7,9 +7,13 @@ from ..photosharing.models import Team
 
 
 class Invite(models.Model):
-    invited = models.ForeignKey(User, related_name='invite_invited', verbose_name='User', on_delete=models.CASCADE)
-    inviter = models.ForeignKey(Team, related_name='invite_inviter', on_delete=models.CASCADE)
-    invite_date = models.DateTimeField(verbose_name=('created'), default=timezone.now)
+    invited = models.ForeignKey(User, related_name='invite_invited',
+                                verbose_name='User',
+                                on_delete=models.CASCADE)
+    inviter = models.ForeignKey(Team, related_name='invite_inviter',
+                                on_delete=models.CASCADE)
+    invite_date = models.DateTimeField(verbose_name=('created'),
+                                       default=timezone.now)
 
     class Meta:
         unique_together = (('invited', 'inviter'),)
