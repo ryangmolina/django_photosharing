1253950e74d3ef28de3bddccdbee613538409693
ryangmolina <ryangabrielpmolina@gmail.com>
2018-10-24 21:55:40 +0800

    Add logo and description field for model Team

--- app/photosharing/models.py
+++ app/photosharing/models.py
@@ -12,6 +12,8 @@ class Team(models.Model):
     owner = models.ForeignKey(User, related_name='team_owner', on_delete=models.CASCADE)
     members = models.ManyToManyField(User, related_name='team_members', blank=True)
     name = models.CharField(max_length=64)
+    description = models.CharField(max_length=250, blank=True)
+    logo = models.ImageField(upload_to=settings.MEDIA_URL, storage=fs, blank=True, null=True)
     establish_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -25,6 +27,7 @@ class Photo(models.Model):
     uploaded_to = models.ForeignKey(Team, on_delete=models.CASCADE)
     uploader = models.ForeignKey(User, on_delete=models.CASCADE)
     upload_date = models.DateTimeField(default=timezone.now)
-    photo = models.ImageField(blank=True, null=True, upload_to=settings.MEDIA_URL, storage=fs)
+    photo = models.ImageField(upload_to=settings.MEDIA_URL, storage=fs, blank=True, null=True)
     public = models.BooleanField(default=False)
     description = models.CharField(max_length=200, blank=True, null=True)
+
